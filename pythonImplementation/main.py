e253e8192621a484493805fbcd75cd725f0b0cab
Uni_laptop_ninjahorn <danielgellert09@gmail.com>
2024-08-11 20:15:11 +0200

    Added python implementation for sequential and parallel matrix generation

--- pythonImplementation/main.py
+++ pythonImplementation/main.py
@@ -1,21 +1,43 @@
 import random
 import numpy as np
+from multiprocessing import Pool, cpu_count
 
-def initRandArray(sizeX, sizeY=None):
+def initRandMatrixSequential(sizeX, sizeY=None):
     if sizeY is None:
         sizeY = sizeX
-    a = []
-    for ignore in range(sizeX):
+    matrix = []
+    for _ in range(sizeX):
         line = []
         for y in range(sizeY):
             zahl=random.randint(0,100)/10
             if(zahl==0):
-                zahl = 2.0
+                zahl = 0.1
             line.append(zahl)
-        a.append(line)
-    return a
+        matrix.append(line)
+    return matrix
 
+def generateLine(sizeY):
+    line = []
+    for _ in range(sizeY):
+        zahl = random.randint(0,100)/10
+        if(zahl==0):
+            zahl = 0.1
+        line.append(zahl)
+    return line
 
-matrix1 = initRandArray(3,3)
+def initRandMatrixParallel(sizeX, sizeY=None):
+    if sizeY is None:
+        sizeY = sizeX
 
+    with Pool(processes=cpu_count()) as pool:
+        matrix = pool.map(lambda _: generateLine(sizeY), range(sizeX))
+    
+    return matrix
+
+matrix1 = np.array(initRandMatrixSequential(8))
 print(matrix1)
+
+matrix2 = np.array(initRandMatrixParallel(8))
+print(matrix2)
+
+
